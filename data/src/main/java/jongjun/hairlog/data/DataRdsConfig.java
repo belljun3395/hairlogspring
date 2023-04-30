4a5e94f90c53b5823990e8679302bdec203c269f
belljun3395 <195850@jnu.ac.kr>
2023-04-30 19:26:42 +0900

    feat(app): DataRdsConfig can scan entity

--- data/src/main/java/jongjun/hairlog/data/DataRdsConfig.java
+++ data/src/main/java/jongjun/hairlog/data/DataRdsConfig.java
@@ -1,6 +1,8 @@
 package jongjun.hairlog.data;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EntityScan
 public class DataRdsConfig {}
