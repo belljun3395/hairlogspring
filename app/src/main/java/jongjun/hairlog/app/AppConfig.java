7968fcffd9b644b04cea2c73a508d4c9f402c9a9
belljun3395 <195850@jnu.ac.kr>
2023-05-01 02:10:58 +0900

    feat(app): add JpaDataSourceConfig to AppConfig

--- app/src/main/java/jongjun/hairlog/app/AppConfig.java
+++ app/src/main/java/jongjun/hairlog/app/AppConfig.java
@@ -1,11 +1,12 @@
 package jongjun.hairlog.app;
 
 import jongjun.hairlog.data.DataRdsConfig;
+import jongjun.hairlog.data.JpaDataSourceConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan
-@Import({DataRdsConfig.class})
+@Import({DataRdsConfig.class, JpaDataSourceConfig.class})
 public class AppConfig {}
