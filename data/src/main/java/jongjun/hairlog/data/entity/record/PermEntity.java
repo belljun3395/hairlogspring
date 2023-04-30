e2981569c38ea7e931bfd7cf20c34c687e32fd8e
belljun3395 <195850@jnu.ac.kr>
2023-04-30 20:01:14 +0900

    feat(data): init PermEntity

---
+++ data/src/main/java/jongjun/hairlog/data/entity/record/PermEntity.java
@@ -0,0 +1,34 @@
+package jongjun.hairlog.data.entity.record;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+import jongjun.hairlog.data.enums.HurtRate;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
+@DiscriminatorValue("perm")
+@SuperBuilder
+@Table(name = "record_perm_entity")
+public class PermEntity extends RecordEntity {
+
+	@Column(name = "perm_name")
+	private String permName;
+
+	@Column(name = "perm_time")
+	private Long permTime;
+
+	@Enumerated(value = EnumType.STRING)
+	@Column(name = "perm_hurt")
+	private HurtRate permHurt;
+}
