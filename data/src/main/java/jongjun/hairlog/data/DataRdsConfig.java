69864328c783fdd18b164ad007de082e086bcf8b
belljun3395 <195850@jnu.ac.kr>
2023-04-30 20:32:18 +0900

    feat(data): add DataRdsConfig can use JpaRepositories

--- data/src/main/java/jongjun/hairlog/data/DataRdsConfig.java
+++ data/src/main/java/jongjun/hairlog/data/DataRdsConfig.java
@@ -2,7 +2,9 @@ package jongjun.hairlog.data;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EntityScan
+@EnableJpaRepositories
 public class DataRdsConfig {}
