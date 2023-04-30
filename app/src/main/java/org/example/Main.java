2580c49c983734706298be4820754874c1cd3176
belljun3395 <195850@jnu.ac.kr>
2023-04-30 17:59:40 +0900

    feat(app): init build.gradle

--- app/src/main/java/org/example/Main.java
+++ app/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
 }
