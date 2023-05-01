f19faa0b59348a41e6a580a55f2b3f8a32730765
belljun3395 <195850@jnu.ac.kr>
2023-05-01 21:05:52 +0900

    chore(data): change DesignerCustomQueryImpl entitymanager location

--- data/src/main/java/jongjun/hairlog/data/repository/query/DesignerCustomQueryImpl.java
+++ data/src/main/java/jongjun/hairlog/data/repository/query/DesignerCustomQueryImpl.java
@@ -13,7 +13,6 @@ public class DesignerCustomQueryImpl implements DesignerCustomQuery {
 	private static final String MEMBERID_PARAMETER = "memberId";
 	private static final Integer MEMBERID_NATIVE_PARAMETER = 1;
 	private static final Integer DESIGNERNAME_NATIVE_PARAMETER = 2;
-	private final EntityManager em;
 
 	/** 일반 fk로 조회하는 경우랑 성능 비교해보기 */
 	private static final String DESIGNER_FINDBY_MEMBERID =
@@ -22,6 +21,8 @@ public class DesignerCustomQueryImpl implements DesignerCustomQuery {
 
 	private static final String DESIGNER_SEARCHBY_NAME_MEMBERID =
 			"select * from designer_entity d where d.member_fk = ?1 and match(designer_name) against(?2 in natural language mode)";
+
+	private final EntityManager em;
 
 	public List<DesignerEntity> findByMemberIdQuery(Long memberId) {
 		return em.createQuery(DESIGNER_FINDBY_MEMBERID, DesignerEntity.class)
