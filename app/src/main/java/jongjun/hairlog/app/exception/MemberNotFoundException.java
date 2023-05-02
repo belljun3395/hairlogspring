b4d050db684f6db306239e05ee819b630aed0c1b
belljun3395 <195850@jnu.ac.kr>
2023-05-02 15:54:13 +0900

    feat(app): add MemberNotFoundException email case

--- app/src/main/java/jongjun/hairlog/app/exception/MemberNotFoundException.java
+++ app/src/main/java/jongjun/hairlog/app/exception/MemberNotFoundException.java
@@ -1,15 +1,24 @@
 package jongjun.hairlog.app.exception;
 
+import java.util.Objects;
+
 /** memberId 로 회원 조회 실패시 발생하는 예외 */
 public class MemberNotFoundException extends RuntimeException {
-	private final Long memberId;
+	private Long memberId;
+	private String email;
 
 	public MemberNotFoundException(Long memberId) {
 		this.memberId = memberId;
 	}
 
+	public MemberNotFoundException(String email) {
+		this.email = email;
+	}
+
 	@Override
 	public String getMessage() {
-		return "존재하지 않는 회원입니다. memberId: " + memberId;
+		return Objects.isNull(email)
+				? "존재하지 않는 회원입니다. memberId: " + memberId
+				: "존재하지 않는 회원입니다. email : " + email;
 	}
 }
