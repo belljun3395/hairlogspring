ff296d2655c3c8bd6b58158848ce179fe9b204fa
belljun3395 <195850@jnu.ac.kr>
2023-05-03 00:15:18 +0900

    fix(data): add required args constructor for native query

--- data/src/main/java/jongjun/hairlog/data/dto/record/RecordDeletedDTO.java
+++ data/src/main/java/jongjun/hairlog/data/dto/record/RecordDeletedDTO.java
@@ -1,5 +1,6 @@
 package jongjun.hairlog.data.dto.record;
 
+import java.math.BigInteger;
 import java.util.Date;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -10,4 +11,9 @@ public class RecordDeletedDTO {
 
 	private final Long recordId;
 	private final Date recordDate;
+
+	public RecordDeletedDTO(BigInteger recordId, Date recordDate) {
+		this.recordId = Long.valueOf(recordId.toString());
+		this.recordDate = recordDate;
+	}
 }
