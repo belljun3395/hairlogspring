493f9d67fd1cf16e9d21123dd6e81e024e94e99a
belljun3395 <195850@jnu.ac.kr>
2023-05-04 12:15:05 +0900

    feat(data): add RequiredArgsConstructor to MemberDeletedDTO

--- data/src/main/java/jongjun/hairlog/data/dto/member/MemberDeletedDTO.java
+++ data/src/main/java/jongjun/hairlog/data/dto/member/MemberDeletedDTO.java
@@ -2,8 +2,10 @@ package jongjun.hairlog.data.dto.member;
 
 import jongjun.hairlog.data.enums.MemberSex;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class MemberDeletedDTO {
 
 	private final String email;
