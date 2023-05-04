a8c926c616b22b51e5c4de09db6c2630433fc186
belljun3395 <195850@jnu.ac.kr>
2023-05-05 01:01:00 +0900

    feat(app): init Token model

---
+++ app/src/main/java/jongjun/hairlog/app/domain/model/member/Token.java
@@ -0,0 +1,16 @@
+package jongjun.hairlog.app.domain.model.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Token {
+
+	private String accessToken;
+	private String refreshToken;
+}
