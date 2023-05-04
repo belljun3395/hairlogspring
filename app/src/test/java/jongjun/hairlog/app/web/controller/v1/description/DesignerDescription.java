ccd65457ef930c10592c75f87d1e2f19a242b43a
belljun3395 <195850@jnu.ac.kr>
2023-05-04 17:07:37 +0900

    test(app): add DesignerDescription designer

--- app/src/test/java/jongjun/hairlog/app/web/controller/v1/description/DesignerDescription.java
+++ app/src/test/java/jongjun/hairlog/app/web/controller/v1/description/DesignerDescription.java
@@ -17,6 +17,16 @@ public class DesignerDescription {
 		};
 	}
 
+	public static FieldDescriptor[] designer() {
+		return new FieldDescriptor[] {
+			fieldWithPath("data").type(JsonFieldType.OBJECT).description("디자이너"),
+			fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("디자이너 id"),
+			fieldWithPath("data.designerName").type(JsonFieldType.STRING).description("디자이너 이름"),
+			fieldWithPath("data.designerSalon").type(JsonFieldType.STRING).description("디자이너 미용실"),
+			fieldWithPath("data.designerFav").type(JsonFieldType.BOOLEAN).description("디자이너 선호"),
+		};
+	}
+
 	public static FieldDescriptor[] saveDesigner() {
 		return new FieldDescriptor[] {
 			fieldWithPath("data").type(JsonFieldType.NUMBER).description("디자이너 id"),
