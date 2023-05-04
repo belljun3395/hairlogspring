e28c1bc5ab7408bdd9b70ead137eaa9ef61485ae
belljun3395 <195850@jnu.ac.kr>
2023-05-04 17:05:44 +0900

    feat(app): add GetDesignerUseCase for DesignerIdParam execute

--- app/src/main/java/jongjun/hairlog/app/domain/usecase/designer/GetDesignerUseCase.java
+++ app/src/main/java/jongjun/hairlog/app/domain/usecase/designer/GetDesignerUseCase.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 import jongjun.hairlog.app.domain.converter.designer.DesignerConverter;
 import jongjun.hairlog.app.domain.model.designer.Designer;
+import jongjun.hairlog.app.exception.ResourceNotFoundException;
+import jongjun.hairlog.app.web.controller.request.designer.DesignerIdParam;
 import jongjun.hairlog.data.repository.DesignerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,6 +23,13 @@ public class GetDesignerUseCase {
 		return repository.findAllByMemberIdQuery(memberId).stream()
 				.map(converter::from)
 				.collect(Collectors.toList());
+	}
+
+	public Designer execute(DesignerIdParam requestParam) {
+		return repository
+				.findById(requestParam.getDesignerId())
+				.map(converter::from)
+				.orElseThrow(() -> new ResourceNotFoundException("not found designer"));
 	}
 
 	public List<Designer> execute(Long memberId, String designerName) {
