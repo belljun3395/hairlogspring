7ae009b6eef569038b41a785e088c0f2eb249569
belljun3395 <195850@jnu.ac.kr>
2023-05-04 12:11:40 +0900

    test(app): init DesignerDescription

---
+++ app/src/test/java/jongjun/hairlog/app/web/controller/v1/description/DesignerDescription.java
@@ -0,0 +1,25 @@
+package jongjun.hairlog.app.web.controller.v1.description;
+
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+
+import org.springframework.restdocs.payload.FieldDescriptor;
+import org.springframework.restdocs.payload.JsonFieldType;
+
+public class DesignerDescription {
+
+	public static FieldDescriptor[] designers() {
+		return new FieldDescriptor[] {
+			fieldWithPath("data[]").type(JsonFieldType.ARRAY).description("디자이너"),
+			fieldWithPath("data[].id").type(JsonFieldType.NUMBER).description("디자이너 id"),
+			fieldWithPath("data[].designerName").type(JsonFieldType.STRING).description("디자이너 이름"),
+			fieldWithPath("data[].designerSalon").type(JsonFieldType.STRING).description("디자이너 미용실"),
+			fieldWithPath("data[].designerFav").type(JsonFieldType.BOOLEAN).description("디자이너 선호"),
+		};
+	}
+
+	public static FieldDescriptor[] saveDesigner() {
+		return new FieldDescriptor[] {
+			fieldWithPath("data").type(JsonFieldType.NUMBER).description("디자이너 id"),
+		};
+	}
+}
