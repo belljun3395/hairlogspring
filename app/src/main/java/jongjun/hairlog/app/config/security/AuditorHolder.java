90e856f6d1a6c826e2ab00206d411dbdc91498e0
belljun3395 <195850@jnu.ac.kr>
2023-05-06 00:11:05 +0900

    feat(app): init AuditorHolder

---
+++ app/src/main/java/jongjun/hairlog/app/config/security/AuditorHolder.java
@@ -0,0 +1,21 @@
+package jongjun.hairlog.app.config.security;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@UtilityClass
+public class AuditorHolder {
+
+	public static Long get() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		if (authentication == null
+				|| !authentication.isAuthenticated()
+				|| authentication.getPrincipal().equals("anonymousUser")) {
+			return 0L;
+		}
+
+		return Long.valueOf(String.valueOf(authentication.getPrincipal()));
+	}
+}
