2c8c552cdd4a00845bfb81d5cd75988bb1304200
belljun3395 <195850@jnu.ac.kr>
2023-05-06 00:15:06 +0900

    feat(app): add MemberController refreshToken

--- app/src/main/java/jongjun/hairlog/app/web/controller/v1/MemberController.java
+++ app/src/main/java/jongjun/hairlog/app/web/controller/v1/MemberController.java
@@ -2,7 +2,9 @@ package jongjun.hairlog.app.web.controller.v1;
 
 import jongjun.hairlog.app.domain.model.member.Member;
 import jongjun.hairlog.app.domain.model.member.MemberInfo;
+import jongjun.hairlog.app.domain.model.member.Token;
 import jongjun.hairlog.app.domain.usecase.member.GetMemberUseCase;
+import jongjun.hairlog.app.domain.usecase.member.GetTokenUseCase;
 import jongjun.hairlog.app.domain.usecase.member.SaveMemberUseCase;
 import jongjun.hairlog.app.support.ApiResponse;
 import jongjun.hairlog.app.support.ApiResponseGenerator;
@@ -12,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,10 +26,17 @@ public class MemberController {
 
 	private final SaveMemberUseCase saveMemberUseCase;
 	private final GetMemberUseCase getMemberUseCase;
+	private final GetTokenUseCase getTokenUseCase;
 
 	@PostMapping()
 	public ApiResponse<ApiResponse.SuccessBody<Long>> addMember(@RequestBody MemberRequest request) {
 		return ApiResponseGenerator.success(saveMemberUseCase.execute(request), HttpStatus.OK);
+	}
+
+	@PostMapping("/tokens")
+	public ApiResponse<ApiResponse.SuccessBody<Token>> refreshToken(
+			@RequestHeader("X-REFRESH-TOKEN") String refreshToken) {
+		return ApiResponseGenerator.success(getTokenUseCase.execute(refreshToken), HttpStatus.CREATED);
 	}
 
 	@GetMapping()
