b0d72bbc5477b67ef79a1e5094264760c3cb4d6b
belljun3395 <195850@jnu.ac.kr>
2023-05-06 00:13:18 +0900

    feat(app): add ComponentScan BasePackage

--- app/src/main/java/jongjun/hairlog/app/config/AppConfig.java
+++ app/src/main/java/jongjun/hairlog/app/config/AppConfig.java
@@ -1,5 +1,7 @@
 package jongjun.hairlog.app.config;
 
+import static jongjun.hairlog.app.config.AppConfig.APP_BASE_PACKAGE;
+
 import jongjun.hairlog.data.DataRdsConfig;
 import jongjun.hairlog.data.JpaDataSourceConfig;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,6 +9,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@ComponentScan
+@ComponentScan(value = {APP_BASE_PACKAGE})
 @Import({DataRdsConfig.class, JpaDataSourceConfig.class})
-public class AppConfig {}
+public class AppConfig {
+
+	public static final String APP_BASE_PACKAGE = "jongjun.hairlog.app";
+}
