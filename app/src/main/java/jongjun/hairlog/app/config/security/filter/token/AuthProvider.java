9f447b03fe1cb1366d9adf37f151bcbbae1fc7aa
belljun3395 <195850@jnu.ac.kr>
2023-05-06 00:09:41 +0900

    feat(app): init AuthProvider

---
+++ app/src/main/java/jongjun/hairlog/app/config/security/filter/token/AuthProvider.java
@@ -0,0 +1,53 @@
+package jongjun.hairlog.app.config.security.filter.token;
+
+import java.util.Collections;
+import java.util.Optional;
+import jongjun.hairlog.app.support.token.TokenResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AuthProvider implements AuthenticationProvider {
+
+	private static final String ROLE_USER = "ROLE_USER";
+	private static final String PREAUTH_TOKEN_CREDENTIAL = "";
+
+	private final TokenResolver tokenResolver;
+
+	@Override
+	public Authentication authenticate(Authentication authentication)
+			throws AuthenticationException, AccessDeniedException {
+
+		Long memberId =
+				Optional.ofNullable(authentication.getPrincipal())
+						.map(String.class::cast)
+						.flatMap(tokenResolver::resolveToken)
+						.orElseThrow(
+								() ->
+										new AccessTokenInvalidException(
+												"Invalid access token. accessToken: " + authentication.getPrincipal()));
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		if (authentication instanceof PreAuthenticatedAuthenticationToken) {
+			return new PreAuthenticatedAuthenticationToken(
+					memberId,
+					PREAUTH_TOKEN_CREDENTIAL,
+					Collections.singleton(new SimpleGrantedAuthority(ROLE_USER)));
+		}
+		return null;
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return PreAuthenticatedAuthenticationToken.class.isAssignableFrom(authentication);
+	}
+}
