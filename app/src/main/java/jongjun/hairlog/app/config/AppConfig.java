edef6cbc4f7efefe8f663fde08bb0cee03818dce
belljun3395 <195850@jnu.ac.kr>
2023-05-06 14:48:51 +0900

    feat(app): add AspectJProxy config

--- app/src/main/java/jongjun/hairlog/app/config/AppConfig.java
+++ app/src/main/java/jongjun/hairlog/app/config/AppConfig.java
@@ -6,10 +6,12 @@ import jongjun.hairlog.data.DataRdsConfig;
 import jongjun.hairlog.data.JpaDataSourceConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan(value = {APP_BASE_PACKAGE})
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 @Import({DataRdsConfig.class, JpaDataSourceConfig.class})
 public class AppConfig {
 
