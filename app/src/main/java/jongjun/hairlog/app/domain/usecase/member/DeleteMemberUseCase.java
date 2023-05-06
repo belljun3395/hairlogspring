1baf7e8101fef6a1d1a0dbaacd73496eb70968f3
belljun3395 <195850@jnu.ac.kr>
2023-05-06 15:53:55 +0900

    feat(app): init DeleteMemberUseCase

---
+++ app/src/main/java/jongjun/hairlog/app/domain/usecase/member/DeleteMemberUseCase.java
@@ -0,0 +1,25 @@
+package jongjun.hairlog.app.domain.usecase.member;
+
+import jongjun.hairlog.app.exception.MemberNotFoundException;
+import jongjun.hairlog.app.support.aop.ValidateRequestMemberId;
+import jongjun.hairlog.data.entity.MemberEntity;
+import jongjun.hairlog.data.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class DeleteMemberUseCase {
+
+	private final MemberRepository repository;
+
+	@ValidateRequestMemberId
+	public Long execute(Long memberId) {
+		MemberEntity memberEntity =
+				repository.findById(memberId).orElseThrow(() -> new MemberNotFoundException(memberId));
+		repository.delete(memberEntity);
+		return memberEntity.getId();
+	}
+}
