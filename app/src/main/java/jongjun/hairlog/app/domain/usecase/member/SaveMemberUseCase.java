517c0dad86b86fc865f3616ea79312b3e6ef969e
belljun3395 <195850@jnu.ac.kr>
2023-05-06 15:33:58 +0900

    feat(app): add SaveMemberUseCase editMember execute

--- app/src/main/java/jongjun/hairlog/app/domain/usecase/member/SaveMemberUseCase.java
+++ app/src/main/java/jongjun/hairlog/app/domain/usecase/member/SaveMemberUseCase.java
@@ -1,6 +1,10 @@
 package jongjun.hairlog.app.domain.usecase.member;
 
 import jongjun.hairlog.app.domain.converter.member.MemberConverter;
+import jongjun.hairlog.app.domain.converter.member.MemberUpdateConverter;
+import jongjun.hairlog.app.exception.MemberNotFoundException;
+import jongjun.hairlog.app.support.aop.ValidateRequestMemberId;
+import jongjun.hairlog.app.web.controller.request.member.MemberEditRequest;
 import jongjun.hairlog.app.web.controller.request.member.MemberRequest;
 import jongjun.hairlog.data.entity.MemberEntity;
 import jongjun.hairlog.data.repository.MemberRepository;
@@ -15,10 +19,19 @@ public class SaveMemberUseCase {
 
 	private final MemberRepository repository;
 	private final MemberConverter converter;
+	private final MemberUpdateConverter updateConverter;
 
 	public Long execute(MemberRequest request) {
 		MemberEntity data = converter.to(request);
 		repository.save(data);
 		return data.getId();
+	}
+
+	@ValidateRequestMemberId
+	public Long execute(Long memberId, MemberEditRequest request) {
+		MemberEntity memberEntity =
+				repository.findById(memberId).orElseThrow(() -> new MemberNotFoundException(memberId));
+		updateConverter.to(memberEntity, request);
+		return memberEntity.getId();
 	}
 }
