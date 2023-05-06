07cfab985e8f4b568a2cae22ca0a811e38efb259
belljun3395 <195850@jnu.ac.kr>
2023-05-06 17:55:30 +0900

    feat(app): add SaveRecordUseCase edit records execute

--- app/src/main/java/jongjun/hairlog/app/domain/usecase/record/SaveRecordUseCase.java
+++ app/src/main/java/jongjun/hairlog/app/domain/usecase/record/SaveRecordUseCase.java
@@ -1,8 +1,15 @@
 package jongjun.hairlog.app.domain.usecase.record;
 
 import jongjun.hairlog.app.domain.converter.record.RecordConverter;
+import jongjun.hairlog.app.domain.converter.record.RecordUpdateConverter;
+import jongjun.hairlog.app.exception.ResourceNotFoundException;
+import jongjun.hairlog.app.support.aop.ValidateRequestMemberId;
+import jongjun.hairlog.app.support.validate.SourceValidator;
+import jongjun.hairlog.app.web.controller.request.record.CutRecordEditRequest;
 import jongjun.hairlog.app.web.controller.request.record.CutRecordRequest;
+import jongjun.hairlog.app.web.controller.request.record.DyeingRecordEditRequest;
 import jongjun.hairlog.app.web.controller.request.record.DyeingRecordRequest;
+import jongjun.hairlog.app.web.controller.request.record.PermRecordEditRequest;
 import jongjun.hairlog.app.web.controller.request.record.PermRecordRequest;
 import jongjun.hairlog.data.entity.record.CutEntity;
 import jongjun.hairlog.data.entity.record.DyeingEntity;
@@ -19,6 +26,7 @@ public class SaveRecordUseCase {
 
 	private final RecordRepository repository;
 	private final RecordConverter converter;
+	private final RecordUpdateConverter updateConverter;
 
 	public Long execute(CutRecordRequest request) {
 		CutEntity data = converter.to(request);
@@ -36,5 +44,50 @@ public class SaveRecordUseCase {
 		DyeingEntity data = converter.to(request);
 		repository.save(data);
 		return data.getId();
+	}
+
+	@ValidateRequestMemberId
+	public Long execute(Long memberId, CutRecordEditRequest request) {
+		CutEntity source =
+				(CutEntity)
+						repository
+								.findById(request.getId())
+								.orElseThrow(() -> new ResourceNotFoundException("not found record"));
+
+		SourceValidator.validate(memberId, source);
+
+		updateConverter.to(source, request);
+
+		return source.getId();
+	}
+
+	@ValidateRequestMemberId
+	public Long execute(Long memberId, PermRecordEditRequest request) {
+		PermEntity source =
+				(PermEntity)
+						repository
+								.findById(request.getId())
+								.orElseThrow(() -> new ResourceNotFoundException("not found record"));
+
+		SourceValidator.validate(memberId, source);
+
+		updateConverter.to(source, request);
+
+		return source.getId();
+	}
+
+	@ValidateRequestMemberId
+	public Long execute(Long memberId, DyeingRecordEditRequest request) {
+		DyeingEntity source =
+				(DyeingEntity)
+						repository
+								.findById(request.getId())
+								.orElseThrow(() -> new ResourceNotFoundException("not found record"));
+
+		SourceValidator.validate(memberId, source);
+
+		updateConverter.to(source, request);
+
+		return source.getId();
 	}
 }
