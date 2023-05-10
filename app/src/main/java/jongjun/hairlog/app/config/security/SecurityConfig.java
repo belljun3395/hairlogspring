bedadf80a96dc73a7ee6be272ccfc8986a7ffa7f
belljun3395 <195850@jnu.ac.kr>
2023-05-10 23:21:07 +0900

    feat(app): add !prod security config for cors

--- app/src/main/java/jongjun/hairlog/app/config/security/SecurityConfig.java
+++ app/src/main/java/jongjun/hairlog/app/config/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package jongjun.hairlog.app.config.security;
 
+import java.util.Arrays;
+import java.util.List;
 import jongjun.hairlog.app.config.security.filter.token.AuthProvider;
 import jongjun.hairlog.app.config.security.filter.token.AuthenticationFilter;
 import jongjun.hairlog.app.config.security.handler.DelegatedAccessDeniedHandler;
@@ -7,6 +9,7 @@ import jongjun.hairlog.app.config.security.handler.DelegatedAuthenticationEntryP
 import jongjun.hairlog.app.support.token.TokenResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +17,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -25,10 +31,12 @@ public class SecurityConfig {
 	private final TokenResolver tokenResolver;
 
 	@Bean
+	@Profile("!prod")
 	public SecurityFilterChain localSecurityFilterChain(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.formLogin().disable();
 		http.httpBasic().disable();
+		http.cors();
 		http.authorizeRequests()
 				.antMatchers(
 						HttpMethod.GET,
@@ -60,5 +68,20 @@ public class SecurityConfig {
 		AuthenticationFilter authenticationFilter = new AuthenticationFilter();
 		authenticationFilter.setAuthenticationManager(new ProviderManager(authProvider));
 		return authenticationFilter;
+	}
+
+	@Bean
+	@Profile("!prod")
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		configuration.setAllowedOriginPatterns(List.of("*"));
+		configuration.setAllowedMethods(Arrays.asList("HEAD","POST","GET","DELETE","PUT"));
+		configuration.setAllowedHeaders(List.of("*"));
+		configuration.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
