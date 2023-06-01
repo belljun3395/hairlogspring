e694e04f7eafe3b2721e3540c0aab5a2b31e5150
belljun3395 <195850@jnu.ac.kr>
2023-06-01 21:14:47 +0900

    feat: add TestConfig

---
+++ data/src/test/java/jongjun/hairlog/data/repository/TestConfig.java
@@ -0,0 +1,8 @@
+package jongjun.hairlog.data.repository;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "jongjun.hairlog.data.repository")
+public class TestConfig {}
