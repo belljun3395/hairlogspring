c14d6f079c04831c3d0c6d885c8e7dea776c1c14
belljun3395 <195850@jnu.ac.kr>
2023-08-13 00:50:18 +0900

    refactor(data): MemberInfoView 멤버 요소 추가

--- data/src/main/java/jongjun/hairlog/data/dto/member/MemberInfoView.java
+++ data/src/main/java/jongjun/hairlog/data/dto/member/MemberInfoView.java
@@ -1,6 +1,7 @@
 package jongjun.hairlog.data.dto.member;
 
 import com.querydsl.core.annotations.QueryProjection;
+import jongjun.hairlog.data.enums.MemberSex;
 import lombok.Getter;
 
 @Getter
@@ -8,10 +9,14 @@ public class MemberInfoView {
 
 	private String email;
 	private String name;
+	private MemberSex sex;
+	private Long cycle;
 
 	@QueryProjection
-	public MemberInfoView(String email, String name) {
+	public MemberInfoView(String email, String name, MemberSex sex, Long cycle) {
 		this.email = email;
 		this.name = name;
+		this.sex = sex;
+		this.cycle = cycle;
 	}
 }
