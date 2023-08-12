59cc65079bcda8b2bbcbdca72d57e69d96c6d587
belljun3395 <195850@jnu.ac.kr>
2023-08-12 10:09:49 +0900

    refactor(data): DesignerEntity BaseEntity 상속하는 방식으로 변경 및 @Where을 통한 deleted false 강제 제거

--- data/src/main/java/jongjun/hairlog/data/entity/DesignerEntity.java
+++ data/src/main/java/jongjun/hairlog/data/entity/DesignerEntity.java
@@ -1,55 +1,39 @@
 package jongjun.hairlog.data.entity;
 
-import java.time.LocalDateTime;
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
-import javax.persistence.Convert;
+import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.ForeignKey;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.NamedNativeQuery;
 import javax.persistence.Table;
-import jongjun.hairlog.data.converter.IdConverter;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.hibernate.annotations.DynamicUpdate;
+import lombok.ToString.Exclude;
+import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Entity
-@DynamicUpdate
 @ToString
-@Builder(toBuilder = true)
-@Table(name = "designer_entity")
-@EntityListeners(AuditingEntityListener.class)
-@SQLDelete(sql = "UPDATE designer_entity SET deleted = true WHERE member_fk=?")
-@Where(clause = "deleted=false")
-@NamedNativeQuery(
-		name = "DesignerEntity.findDeletedDesignersEntity",
-		query =
-				"select d.designer_id, d.designer_name, d.designer_salon from designer_entity d where d.member_fk = ?1")
-public class DesignerEntity {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "designer_id")
-	@Convert(converter = IdConverter.class)
-	private Long id;
+@Entity
+@SuperBuilder(toBuilder = true)
+@Table(name = "designer_tb")
+@AttributeOverrides(
+		value = {
+			@AttributeOverride(name = "id", column = @Column(name = "designer_id")),
+		})
+@SQLDelete(sql = "UPDATE designer_tb SET deleted = true WHERE designer_id=?")
+public class DesignerEntity extends BaseEntity {
 
 	@Column(name = "designer_name", nullable = false)
 	private String designerName;
@@ -57,25 +41,14 @@ public class DesignerEntity {
 	@Column(name = "designer_salon")
 	private String designerSalon;
 
-	@Column(name = "designer_fav", nullable = false)
 	@Builder.Default
+	@Column(name = "designer_fav", nullable = false)
 	private Boolean designerFav = true;
 
+	@Exclude
 	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
-	@JoinColumn(name = "member_fk")
+	@JoinColumn(name = "member_fk", foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
 	private MemberEntity member;
-
-	@Column(nullable = false, updatable = false)
-	@CreatedDate
-	private LocalDateTime createAt;
-
-	@Column(nullable = false)
-	@LastModifiedDate
-	private LocalDateTime updateAt;
-
-	@Column(nullable = false)
-	@Builder.Default
-	private Boolean deleted = false;
 
 	public void changeDesignerFav(Boolean designerFav) {
 		this.designerFav = designerFav;
