7c12c6f787b1851449faa5999118775d4c82a271
belljun3395 <195850@jnu.ac.kr>
2023-08-12 11:37:28 +0900

    refactor(data): MemberEntity BaseEntity 상속하는 방식으로 변경 및 @Where을 통한 deleted false 강제 제거

--- data/src/main/java/jongjun/hairlog/data/entity/MemberEntity.java
+++ data/src/main/java/jongjun/hairlog/data/entity/MemberEntity.java
@@ -1,55 +1,36 @@
 package jongjun.hairlog.data.entity;
 
-import java.time.LocalDateTime;
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
-import javax.persistence.Convert;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.NamedNativeQuery;
 import javax.persistence.Table;
-import jongjun.hairlog.data.converter.IdConverter;
 import jongjun.hairlog.data.enums.MemberSex;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.hibernate.annotations.DynamicUpdate;
+import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Entity
-@DynamicUpdate
 @ToString
-@Builder(toBuilder = true)
-@Table(name = "member_entity")
-@EntityListeners(AuditingEntityListener.class)
-@SQLDelete(sql = "UPDATE member_entity SET deleted = true WHERE member_id=?")
-@Where(clause = "deleted=false")
-@NamedNativeQuery(
-		name = "MemberEntity.findDeletedMemberEntity",
-		query = "select m.member_email, m.member_sex from member_entity m where m.member_email = ?1")
-public class MemberEntity {
+@Entity
+@SuperBuilder(toBuilder = true)
+@Table(name = "member_tb")
+@AttributeOverrides(
+		value = {
+			@AttributeOverride(name = "id", column = @Column(name = "member_id")),
+		})
+@SQLDelete(sql = "UPDATE member_tb SET deleted = true WHERE member_id=?")
+public class MemberEntity extends BaseEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "member_id")
-	@Convert(converter = IdConverter.class)
-	private Long id;
-
-	@Column(name = "member_email", unique = true)
+	@Column(name = "member_email")
 	private String email;
 
 	@Column(name = "member_password", nullable = false)
@@ -64,18 +45,6 @@ public class MemberEntity {
 
 	@Column(name = "member_cycle")
 	private Long cycle;
-
-	@Column(nullable = false, updatable = false)
-	@CreatedDate
-	private LocalDateTime createAt;
-
-	@Column(nullable = false)
-	@LastModifiedDate
-	private LocalDateTime updateAt;
-
-	@Column(nullable = false)
-	@Builder.Default
-	private Boolean deleted = false;
 
 	public void changeName(String name) {
 		this.name = name;
