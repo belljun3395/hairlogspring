5031a959bec4675d14a835dfc5d04f7e5b6cf31d
belljun3395 <195850@jnu.ac.kr>
2023-08-12 10:11:34 +0900

    refactor(data): DesignerCustomQuery 선언 수정

--- data/src/main/java/jongjun/hairlog/data/repository/query/DesignerCustomQuery.java
+++ data/src/main/java/jongjun/hairlog/data/repository/query/DesignerCustomQuery.java
@@ -1,14 +1,3 @@
 package jongjun.hairlog.data.repository.query;
 
-import java.util.List;
-import jongjun.hairlog.data.dto.designer.DesignerDTO;
-import jongjun.hairlog.data.dto.designer.DesignerDeletedDTO;
-
-public interface DesignerCustomQuery {
-
-	List<DesignerDTO> findAllByMemberIdQuery(Long memberId);
-
-	List<DesignerDTO> searchByNameAndMemberIdQuery(String designerName, Long memberId);
-
-	List<DesignerDeletedDTO> findAllDeletedByMemberIdQuery(Long memberId);
-}
+public interface DesignerCustomQuery {}
