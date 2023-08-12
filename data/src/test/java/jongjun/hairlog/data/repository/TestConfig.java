eed9742087bb86c7303b1c85894d058f275727c3
belljun3395 <195850@jnu.ac.kr>
2023-08-12 19:38:47 +0900

    remove(data): TestConfig 삭제

--- data/src/test/java/jongjun/hairlog/data/repository/TestConfig.java
+++
@@ -1,8 +0,0 @@
-package jongjun.hairlog.data.repository;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "jongjun.hairlog.data.repository")
-public class TestConfig {}
