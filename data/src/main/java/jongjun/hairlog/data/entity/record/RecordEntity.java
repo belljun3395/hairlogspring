3d4a7795c81ab6ad0246d3c30acd1fe197ebe81a
belljun3395 <195850@jnu.ac.kr>
2023-08-12 19:17:00 +0900

    refactor(data): RecordEntity BaseEntity 상속하는 방식으로 변경, 상속 방식 Entity 구현 변경, @Where을 통한 deleted false 강제 제거
    - 상속 방식에서 id를 통해 연관으로 수정

--- data/src/main/java/jongjun/hairlog/data/entity/record/RecordEntity.java
+++ data/src/main/java/jongjun/hairlog/data/entity/record/RecordEntity.java
@@ -1,118 +1,74 @@
 package jongjun.hairlog.data.entity.record;
 
-import java.time.LocalDateTime;
-import java.util.Date;
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
+import javax.persistence.ConstraintMode;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.ForeignKey;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.NamedNativeQuery;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import jongjun.hairlog.data.entity.BaseEntity;
 import jongjun.hairlog.data.entity.DesignerEntity;
 import jongjun.hairlog.data.entity.MemberEntity;
-import jongjun.hairlog.data.enums.SatisfactionRate;
+import jongjun.hairlog.data.enums.RecordCategory;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import lombok.ToString.Exclude;
 import lombok.experimental.SuperBuilder;
-import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-/** @implSpec {@link jongjun.hairlog.data.entity.record.RecordEntity} 관련 Entity 경우 상속하여 사용한다. */
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Entity
-@DynamicUpdate
-@Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn(name = "record_category")
 @ToString
 @SuperBuilder(toBuilder = true)
-@Table(name = "record_entity")
-@EntityListeners(AuditingEntityListener.class)
-@SQLDelete(sql = "UPDATE record_entity SET deleted = true WHERE member_fk=?")
-@Where(clause = "deleted=false")
-@NamedNativeQuery(
-		name = "RecordEntity.findDeletedRecordsEntity",
-		query = "select r.record_id, r.record_date from record_entity r where r.member_fk = ?1")
-public abstract class RecordEntity {
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "record_tb")
+@AttributeOverrides(
+		value = {
+			@AttributeOverride(name = "id", column = @Column(name = "record_id")),
+		})
+@SQLDelete(sql = "UPDATE record_tb SET deleted = true WHERE record_id=?")
+public class RecordEntity extends BaseEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "record_id")
-	private Long id;
+	@Embedded private CommonRecordInfo recordInfo;
 
-	@Temporal(TemporalType.DATE)
-	@Column(name = "record_date", nullable = false)
-	private Date recordDate;
-
-	@Column(name = "record_cost", nullable = false)
-	private Long recordCost;
-
-	@Column(name = "record_etc")
-	private String recordEtc;
-
+	@Column(name = "record_category", nullable = false)
 	@Enumerated(value = EnumType.STRING)
-	@Column(name = "record_grade", nullable = false)
-	private SatisfactionRate recordGrade;
+	private RecordCategory recordCategory;
 
+	@Column(name = "sub_record_fk", nullable = false)
+	private Long subId;
+
+	@Exclude
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "member_fk")
+	@JoinColumn(name = "member_fk", foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
 	private MemberEntity member;
 
+	@Exclude
 	@OneToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "designer_fk")
+	@JoinColumn(name = "designer_fk", foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
 	private DesignerEntity designer;
 
-	@Column(nullable = false, updatable = false)
-	@CreatedDate
-	private LocalDateTime createAt;
-
-	@Column(nullable = false)
-	@LastModifiedDate
-	private LocalDateTime updateAt;
-
-	@Column(nullable = false)
-	@Builder.Default
-	private Boolean deleted = false;
-
-	public void changeRecordDate(Date recordDate) {
-		this.recordDate = recordDate;
+	public void changeDesigner(Long designerId) {
+		this.designer = DesignerEntity.builder().id(designerId).build();
 	}
 
-	public void changeRecordCost(Long recordCost) {
-		this.recordCost = recordCost;
-	}
-
-	public void changeRecordEtc(String recordEtc) {
-		this.recordEtc = recordEtc;
-	}
-
-	public void changeRecordGrade(SatisfactionRate recordGrade) {
-		this.recordGrade = recordGrade;
-	}
-
-	public void changeDesigner(DesignerEntity designer) {
-		this.designer = designer;
+	public void associateWithSubRecord(Long subId, RecordCategory recordCategory) {
+		this.subId = subId;
+		this.recordCategory = recordCategory;
 	}
 }
