17861f3663d714d48dea2abae3e206f8cc68622d
belljun3395 <195850@jnu.ac.kr>
2023-08-12 10:12:01 +0900

    refactor(data): DesignerRepository 선언 수정

--- data/src/main/java/jongjun/hairlog/data/repository/DesignerRepository.java
+++ data/src/main/java/jongjun/hairlog/data/repository/DesignerRepository.java
@@ -1,8 +1,9 @@
 package jongjun.hairlog.data.repository;
 
 import jongjun.hairlog.data.entity.DesignerEntity;
+import jongjun.hairlog.data.repository.jpa.DesignerJpaRepository;
 import jongjun.hairlog.data.repository.query.DesignerCustomQuery;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.RepositoryDefinition;
 
-public interface DesignerRepository
-		extends CrudRepository<DesignerEntity, Long>, DesignerCustomQuery {}
+@RepositoryDefinition(domainClass = DesignerEntity.class, idClass = Long.class)
+public interface DesignerRepository extends DesignerJpaRepository, DesignerCustomQuery {}
