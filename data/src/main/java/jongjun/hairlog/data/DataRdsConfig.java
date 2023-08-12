eb796b778d179d5db2781dd7cd805006a09c609a
belljun3395 <195850@jnu.ac.kr>
2023-08-12 10:05:05 +0900

    refactor(data): DataRdsConfig 설정 요소 추가

--- data/src/main/java/jongjun/hairlog/data/DataRdsConfig.java
+++ data/src/main/java/jongjun/hairlog/data/DataRdsConfig.java
@@ -1,6 +1,25 @@
 package jongjun.hairlog.data;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class DataRdsConfig {}
+@ComponentScan(basePackages = DataRdsConfig.BASE_PACKAGE)
+@EnableAutoConfiguration(
+		exclude = {
+			DataSourceAutoConfiguration.class,
+			DataSourceTransactionManagerAutoConfiguration.class,
+			HibernateJpaAutoConfiguration.class,
+		})
+public class DataRdsConfig {
+
+	public static final String BASE_PACKAGE = "jongjun.hairlog.data";
+	public static final String SERVICE_NAME = "hairlog";
+	public static final String MODULE_NAME = "data";
+	public static final String BEAN_NAME_PREFIX = SERVICE_NAME + MODULE_NAME;
+	public static final String PROPERTY_PREFIX = SERVICE_NAME + "." + MODULE_NAME;
+}
