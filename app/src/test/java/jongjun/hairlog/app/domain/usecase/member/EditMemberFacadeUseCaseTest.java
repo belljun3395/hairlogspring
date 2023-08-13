610f367af9264e3755b6dd7e2343a2eb7a0ffd1c
belljun3395 <195850@jnu.ac.kr>
2023-08-13 17:09:19 +0900

    refactor(app): id가 변경되지 않도록 변경함에 따른 수정

--- app/src/test/java/jongjun/hairlog/app/domain/usecase/member/EditMemberFacadeUseCaseTest.java
+++ app/src/test/java/jongjun/hairlog/app/domain/usecase/member/EditMemberFacadeUseCaseTest.java
@@ -1,18 +1,19 @@
 package jongjun.hairlog.app.domain.usecase.member;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import jongjun.hairlog.app.config.AppConfig;
 import jongjun.hairlog.app.domain.request.EditMemberRequest;
-import jongjun.hairlog.app.exception.AlreadyEditException;
 import jongjun.hairlog.app.web.controller.converter.MemberControllerConverter;
 import jongjun.hairlog.app.web.controller.request.member.MemberRequest;
 import jongjun.hairlog.data.enums.MemberSex;
+import jongjun.hairlog.data.log.repository.MemberLogRepository;
 import jongjun.hairlog.data.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -32,12 +33,15 @@ class EditMemberFacadeUseCaseTest {
 	@Autowired GetMemberUseCase getMemberUseCase;
 	@Autowired MemberControllerConverter memberControllerConverter;
 	@Autowired MemberRepository memberRepository;
+	@Autowired MemberLogRepository memberLogRepository;
 
 	@Test
 	void concurrencyTest() throws InterruptedException {
 		String targetEmail = "testEmail";
 		Long targetCycle = 100L;
 		memberRepository.deleteAllInBatch();
+		memberLogRepository.deleteAllInBatch();
+
 		MemberRequest member =
 				MemberRequest.builder()
 						.email(targetEmail)
@@ -52,13 +56,14 @@ class EditMemberFacadeUseCaseTest {
 		ExecutorService executorService = Executors.newFixedThreadPool(32);
 		CountDownLatch latch = new CountDownLatch(threadCount);
 		Random random = new Random();
+		List<Long> randomNumberList = new ArrayList<>();
 
 		for (int i = 0; i < threadCount; i++) {
+			long randomLong = random.nextLong(targetCycle);
+			randomNumberList.add(randomLong);
+			String memberName = "testName";
 			EditMemberRequest editMember =
-					EditMemberRequest.builder()
-							.id(targetMemberId)
-							.cycle(random.nextLong(targetCycle))
-							.build();
+					EditMemberRequest.builder().id(targetMemberId).name(memberName).cycle(randomLong).build();
 			executorService.submit(
 					() -> {
 						try {
@@ -71,8 +76,8 @@ class EditMemberFacadeUseCaseTest {
 
 		latch.await();
 
-		assertThatThrownBy(() -> getMemberUseCase.execute(targetMemberId))
-				.isInstanceOf(AlreadyEditException.class);
-		assertThat(getMemberUseCase.execute(targetEmail).getCycle()).isNotEqualTo(targetCycle);
+		Long cycle = getMemberUseCase.execute(targetMemberId).getCycle();
+
+		assertThat(randomNumberList).contains(cycle);
 	}
 }
