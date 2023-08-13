eb4eca4df78ed5df144935952e62759c21764358
belljun3395 <195850@jnu.ac.kr>
2023-08-14 00:26:12 +0900

    remove(data): 네임드 락을 위한 쿼리 삭제

--- data/src/main/java/jongjun/hairlog/data/repository/jpa/MemberJpaRepository.java
+++ data/src/main/java/jongjun/hairlog/data/repository/jpa/MemberJpaRepository.java
@@ -4,17 +4,10 @@ import java.util.Optional;
 import jongjun.hairlog.data.dto.member.DeletedMemberView;
 import jongjun.hairlog.data.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
 public interface MemberJpaRepository extends JpaRepository<MemberEntity, Long> {
-
-	@Query(value = "select get_lock(:key, 3000)", nativeQuery = true)
-	void getLock(String key);
-
-	@Query(value = "select release_lock(:key)", nativeQuery = true)
-	void releaseLock(String key);
 
 	Optional<DeletedMemberView> findTopByEmailAndDeletedTrueOrderById(String email);
 
