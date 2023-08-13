706648c4d77577ec12063e795eebc0ded27d9b7a
belljun3395 <195850@jnu.ac.kr>
2023-08-13 17:46:33 +0900

    refactor(data): lock timeout 수정

--- data/src/main/java/jongjun/hairlog/data/repository/jpa/MemberJpaRepository.java
+++ data/src/main/java/jongjun/hairlog/data/repository/jpa/MemberJpaRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.repository.NoRepositoryBean;
 @NoRepositoryBean
 public interface MemberJpaRepository extends JpaRepository<MemberEntity, Long> {
 
-	@Query(value = "select get_lock(:key, 30000)", nativeQuery = true)
+	@Query(value = "select get_lock(:key, 3000)", nativeQuery = true)
 	void getLock(String key);
 
 	@Query(value = "select release_lock(:key)", nativeQuery = true)
