a505c26871f2150d5e5b055e689f3c4e798f53de
belljun3395 <195850@jnu.ac.kr>
2023-08-13 15:07:11 +0900

    feat(app): member 수정 동시성 제어를 위한 EditMemberFacadeUseCase 구현

---
+++ app/src/main/java/jongjun/hairlog/app/domain/usecase/member/EditMemberFacadeUseCase.java
@@ -0,0 +1,30 @@
+package jongjun.hairlog.app.domain.usecase.member;
+
+import jongjun.hairlog.app.domain.request.EditMemberRequest;
+import jongjun.hairlog.app.web.controller.response.SaveMemberResponse;
+import jongjun.hairlog.data.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class EditMemberFacadeUseCase {
+
+	private final MemberRepository repository;
+	private final EditMemberUseCase editMemberUseCase;
+
+	@Transactional
+	public SaveMemberResponse execute(final EditMemberRequest request) {
+		try {
+			repository.getLock(request.getId().toString());
+
+			return editMemberUseCase.execute(request);
+		} finally {
+			// todo finally 이전에 AlreadyEditException 에 대한 처리 필요
+			repository.releaseLock(request.getId().toString());
+		}
+	}
+}
