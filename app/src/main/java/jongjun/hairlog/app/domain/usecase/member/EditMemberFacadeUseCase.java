77e2a5c00829b9a4f57f7b1cdc1f4e6fdca5b507
belljun3395 <195850@jnu.ac.kr>
2023-08-14 00:37:29 +0900

    refactor(app): 낙관적 락을 통한 구현으로 수정

--- app/src/main/java/jongjun/hairlog/app/domain/usecase/member/EditMemberFacadeUseCase.java
+++ app/src/main/java/jongjun/hairlog/app/domain/usecase/member/EditMemberFacadeUseCase.java
@@ -1,30 +1,28 @@
 package jongjun.hairlog.app.domain.usecase.member;
 
 import jongjun.hairlog.app.domain.request.EditMemberRequest;
-import jongjun.hairlog.app.web.controller.response.SaveMemberResponse;
-import jongjun.hairlog.data.repository.MemberRepository;
+import jongjun.hairlog.app.exception.AlreadyEditException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class EditMemberFacadeUseCase {
 
-	private final MemberRepository repository;
 	private final EditMemberUseCase editMemberUseCase;
+	private final ApplicationEventPublisher publisher;
 
-	@Transactional
 	public Long execute(final EditMemberRequest request) {
-		try {
-			repository.getLock(request.getId().toString());
-
-			return editMemberUseCase.execute(request);
-		} finally {
-			// todo finally 이전에 AlreadyEditException 에 대한 처리 필요
-			repository.releaseLock(request.getId().toString());
+		while (true) {
+			try {
+				return editMemberUseCase.execute(request);
+			} catch (RuntimeException e) {
+				publisher.publishEvent(request);
+				throw new AlreadyEditException(request.getId());
+			}
 		}
 	}
 }
