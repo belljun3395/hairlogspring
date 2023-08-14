200f9ffe59f7b485e8d9f077d2894d85b145e35d
belljun3395 <195850@jnu.ac.kr>
2023-08-15 01:50:59 +0900

    feat(data): findAllByMemberIdAndDeletedTrue,findAllByDesignerNameAndMemberAndDeletedFalse 선언 및 구현

--- data/src/main/java/jongjun/hairlog/data/repository/query/DesignerCustomQuery.java
+++ data/src/main/java/jongjun/hairlog/data/repository/query/DesignerCustomQuery.java
@@ -1,3 +1,13 @@
 package jongjun.hairlog.data.repository.query;
 
-public interface DesignerCustomQuery {}
+import java.util.List;
+import jongjun.hairlog.data.dto.designer.DeletedDesignerView;
+import jongjun.hairlog.data.dto.designer.DesignerView;
+
+public interface DesignerCustomQuery {
+
+	List<DeletedDesignerView> findAllByMemberIdAndDeletedTrue(Long memberId);
+
+	List<DesignerView> findAllByDesignerNameAndMemberAndDeletedFalse(
+			String designerName, Long memberId);
+}
--- data/src/main/java/jongjun/hairlog/data/repository/query/DesignerCustomQueryImpl.java
+++ data/src/main/java/jongjun/hairlog/data/repository/query/DesignerCustomQueryImpl.java
@@ -1,6 +1,12 @@
 package jongjun.hairlog.data.repository.query;
 
+import java.util.List;
+import jongjun.hairlog.data.dto.designer.DeletedDesignerView;
+import jongjun.hairlog.data.dto.designer.DesignerView;
+import jongjun.hairlog.data.dto.designer.QDeletedDesignerView;
+import jongjun.hairlog.data.dto.designer.QDesignerView;
 import jongjun.hairlog.data.entity.DesignerEntity;
+import jongjun.hairlog.data.entity.QDesignerEntity;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +15,34 @@ public class DesignerCustomQueryImpl extends QuerydslRepositorySupport
 		implements DesignerCustomQuery {
 	public DesignerCustomQueryImpl() {
 		super(DesignerEntity.class);
+	}
+
+	@Override
+	public List<DeletedDesignerView> findAllByMemberIdAndDeletedTrue(Long memberId) {
+		QDesignerEntity designerEntity = QDesignerEntity.designerEntity;
+		return from(designerEntity)
+				.select(
+						new QDeletedDesignerView(
+								designerEntity.id, designerEntity.designerName, designerEntity.designerSalon))
+				.where(designerEntity.member.id.eq(memberId), designerEntity.deleted.isTrue())
+				.fetch();
+	}
+
+	@Override
+	public List<DesignerView> findAllByDesignerNameAndMemberAndDeletedFalse(
+			String designerName, Long memberId) {
+		QDesignerEntity designerEntity = QDesignerEntity.designerEntity;
+		return from(designerEntity)
+				.select(
+						new QDesignerView(
+								designerEntity.id,
+								designerEntity.designerName,
+								designerEntity.designerSalon,
+								designerEntity.designerFav))
+				.where(
+						designerEntity.member.id.eq(memberId),
+						designerEntity.deleted.isFalse(),
+						designerEntity.designerName.contains(designerName))
+				.fetch();
 	}
 }
