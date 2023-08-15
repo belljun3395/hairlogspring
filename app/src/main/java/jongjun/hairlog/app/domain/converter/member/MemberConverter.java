076b1bebdd71f7ceea437da039642b559a49cb74
belljun3395 <195850@jnu.ac.kr>
2023-08-15 13:12:33 +0900

    remove(data): 불필요 view 삭제

--- app/src/main/java/jongjun/hairlog/app/domain/converter/member/MemberConverter.java
+++ app/src/main/java/jongjun/hairlog/app/domain/converter/member/MemberConverter.java
@@ -1,7 +1,6 @@
 package jongjun.hairlog.app.domain.converter.member;
 
 import jongjun.hairlog.app.domain.model.member.Member;
-import jongjun.hairlog.app.domain.model.member.MemberAuthInfo;
 import jongjun.hairlog.app.domain.model.member.MemberInfo;
 import jongjun.hairlog.app.domain.request.SaveMemberRequest;
 import jongjun.hairlog.data.dto.member.MemberAuthInfoView;
